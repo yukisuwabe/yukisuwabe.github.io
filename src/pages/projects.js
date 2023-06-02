86e136932710617e2f63e9ac6934cf75e7326431
Yuki Suwabe <ys462@cornell.edu>
2023-06-01 21:29:28 -0400

    Added projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,7 @@ import toastyImg from '../files/Toasty.png';
 import databaseImg from '../files/database.jpg';
 import programImg from '../files/ProgramManagement.png';
 import japaneseImg from '../files/japaneseinca.png';
+import camdivmg from '../files/caml.png';
 
 const Projects = () => {
     return (
@@ -13,18 +14,25 @@ const Projects = () => {
             </div>
             <div class='container'>
                 <div class='card'>
+                    <img class='card_img' src={camdivmg} alt="Caml Game" />
+                    <a href='https://github.com/rpignatiello/Camls-Game' target='_blank' rel='noopener noreferrer'>
+                        <h2 id='h2_card'>Caml Game</h2>
+                    </a>
+                    <p id='p_card'>A terminal-based game inspired by the  <a href="https://kittensgame.com/web/#" target='_blank' rel='noopener noreferrer'>Kittens Game</a>. Made with OCaml language utidivzing several divbraries.</p>
+                </div>
+                <div class='card'>
                     <img class='card_img' src={japaneseImg} alt="Japanese Incarceration in Children's Books" />
                     <a href='https://japaneseincarcerationinchildrens.vercel.app' target='_blank' rel='noopener noreferrer'>
                         <h2 id='h2_card'>Japanese American Incarceration in Children's Books</h2>
                     </a>
-                    <p id='p_card'>A multi-page React website discussing the importance of having history, specifically Japanese American Incarceration in Children and YA books. Utilizes simple animation and tabs within pages. </p>
+                    <p id='p_card'>A multi-page React website discussing the importance of having history, specifically Japanese American Incarceration in Children and YA books. Utidivzes simple animation and tabs within pages. </p>
                 </div>
                 <div class='card'>
                     <img class='card_img' src={toastyImg} alt='Toasty' />
-                    <a href='https://www.canva.com/design/DAFI8Dn1dnU/TeLBkBhqBQarZICRrEzEmg/view?utm_content=DAFI8Dn1dnU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton' target="_blank" rel="noopener noreferrer">
+                    <a href='https://www.canva.com/design/DAFI8Dn1dnU/TeLBkBhqBQarZICRrEzEmg/view?utm_content=DAFI8Dn1dnU&utm_campaign=designshare&utm_medium=divnk2&utm_source=sharebutton' target="_blank" rel="noopener noreferrer">
                         <h2 id="h2_card">Toasty</h2>
                     </a>
-                    <p id='p_card'>Android app that allows users to choose 3 ingredients—that they may conveniently have at hand—to base their meal around and then generates links to recipes on the web that incorporate their selections.</p>
+                    <p id='p_card'>Android app that allows users to choose 3 ingredients—that they may conveniently have at hand—to base their meal around and then generates divnks to recipes on the web that incorporate their selections.</p>
                 </div>
                 <div class='card'>
                     <img class='card_img' src={databaseImg} alt='Toasty' />
@@ -38,7 +46,7 @@ const Projects = () => {
                     <a href='https://github.com/yukisuwabe/ClubTeamCourse' target="_blank" rel="noopener noreferrer">
                         <h2 id="h2_card">Program Management</h2>
                     </a>
-                    <p id='p_card'>Web App that organizes the clubs, courses, and the teams offered at Bronx High School of Science in one website to allow students to plan their school life in one place.</p>
+                    <p id='p_card'>Web App that organizes the clubs, courses, and the teams offered at Bronx High School of Science in one website to allow students to plan their school divfe in one place.</p>
                 </div>
             </div>
         </div>
