a6f782b9affb59e14be817ccb2ceacbb62f18178
Yuki Suwabe <ys462@cornell.edu>
2024-10-10 12:07:20 -0400

    Update oct 2024

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,10 +14,9 @@ const About = () => {
                     <h2>Yuki Suwabe</h2>
                     <p>Cornell University, Class of 2025</p>
                     <p>Birthday: August 12, 2003</p>
-                    <p>Birthplace: Tokyo, Japan</p>
                 </div>
                 <div class="column" id='rightStuff'>
-                    <p id='aboutMeP'>Hi! My name is Yuki Suwabe and I am currently a junior in Cornell Unviersity, majoring in Computer Science at the College of Engineering. I am also an incoming TIP intern at Capital One for Summer 2024! I am interested in Software Engineering, mainly the backend side, but still learning a lot in general!
+                    <p id='aboutMeP'>Hi! My name is Yuki Suwabe and I am a senior in Cornell Unviersity, majoring in Computer Science at the College of Engineering. I was a TIP intern at Capital One for Summer 2024 as well! I am interested in Software Engineering, mainly the backend side, and work as a research assistant in the Human-Computer Interaction field!
                         On campus I am the Executive Co-Director for the BigRed//Hacks organization where we organize the longest student-run hackathon on campus.</p>
                     <p id='aboutMeP'>I was born in Japan and raised in New York City. I became interested in Computer Science when I participated in a Girls Who Code program at Columbia University in middle school. Since then, I've worked on multiple projects as well as participated in Software Design programs over the summer.</p>
                     <p id='aboutMeP'>I love to watch ballet, play video games, and watch Japanese TV shows during my free time!</p>
